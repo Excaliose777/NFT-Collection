f3946265f62db87f47602f2caefa5be99670eade
Alaminxab <alaminxab@gmail.com>
2022-04-07 00:35:29 +0200

    new

---
+++ hardhat/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x2EB04457EFCf2facDb2E3Ef95E5B476eF775AA90";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
